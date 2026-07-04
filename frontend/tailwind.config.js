0f0e0facb54bb1dbdb266ecb37c2fa2432edde86
sayako21 <63695015+sayako21@users.noreply.github.com>
2026-07-04 23:06:47 +0900

    30 触り心地がいいアニメーションを追加 (#36)
    Co-authored-by: sayako21 <sayako.o21@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,21 @@ module.exports = {
     "./public/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-5px)' }
+        },
+        pingSlow: {
+          '75%, 100%': { transform: 'scale(5)', opacity: '0' }
+        }
+      },
+      animation: {
+        'float': 'float 3s ease-in-out infinite',
+        'ping-slow': 'pingSlow 2.5s cubic-bezier(0, 0, 0.2, 1) infinite'
+      }
+    },
   },
   plugins: [],
 }
